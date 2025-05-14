b3c68c0fd0a2187ce69e842007f7ac71fe746568
almamun528 <ayapths@gmail.com>
2025-05-14 20:34:25 +0600

    add navbar

--- SkillSprint-Client/src/component/student/Navbar.jsx
+++ SkillSprint-Client/src/component/student/Navbar.jsx
@@ -1,12 +1,44 @@
-import React from 'react'
-  import websiteLogo from '../../assets/logo.png'
-  // import websiteLogoBG from "../../assets/logoBG.png"
+import React from "react";
+import websiteLogo from "../../assets/logo.png";
+
+// import websiteLogoBG from "../../assets/logoBG.png"
 const Navbar = () => {
   return (
-    <section>
-  
+    <section className="sticky top-0 z-50 ">
+      <div className=" navbar bg-gray-100 shadow-sm ">
+        <div className="flex-1">
+          <img className="w-24 cursor-pointer" src={websiteLogo} alt="" />
+        </div>
+        <div className="flex-none">
+          <div className="dropdown dropdown-end">
+            <div className="dropdown dropdown-end">
+              <button className="mx-2">Become Tutor</button>
+              <div
+                tabIndex={0}
+                type="button"
+                className="btn btn-ghost btn-circle avatar"
+              >
+                <div className="w-10 rounded-full">
+                  <img
+                    alt="Tailwind CSS Navbar component"
+                    src="https://img.daisyui.com/images/stock/photo-1534528741775-53994a69daeb.webp"
+                  />
+                </div>
+              </div>
+              <ul
+                tabIndex={0}
+                className="menu menu-sm dropdown-content bg-base-100 rounded-box z-1 mt-3 w-52 p-2 shadow"
+              >
+                <li>Profile</li>
+
+                <li>Logout</li>
+              </ul>
+            </div>
+          </div>
+        </div>
+      </div>
     </section>
   );
-}
+};
 
-export default Navbar
+export default Navbar;
--- SkillSprint-Client/src/pages/student/Home.jsx
+++ SkillSprint-Client/src/pages/student/Home.jsx
@@ -2,14 +2,117 @@ import Slider from "../../component/Slider/Slider";
 import Navbar from "../../component/student/Navbar";
 
 const Home = () => {
-
   return (
     <section>
       {/* slider  */}
-      <Navbar/>
+      <Navbar />
       <Slider />
-
-
+      <div>
+        <p>
+          Lorem ipsum dolor, sit amet consectetur adipisicing elit. Accusantium
+          nostrum officiis provident quos nihil dicta rerum, minima debitis
+          exercitationem explicabo ex praesentium perferendis porro numquam
+          earum aspernatur sed, molestias quis recusandae. Ullam ratione
+          tempore, amet pariatur sed nulla totam minima eius a corporis quod,
+          dolorum magni cum quam, optio facere officia debitis soluta quia
+          deserunt! Error ex dolorum, incidunt quasi optio et sunt iusto est
+          similique impedit illum. Voluptates dolorum mollitia dignissimos non
+          natus, blanditiis ad molestias ut officia incidunt nihil magnam enim.
+          Explicabo rerum totam pariatur eaque minima impedit quisquam. Porro
+          alias sit sint placeat. Ut labore tempore in.
+        </p>
+        <p>
+          Lorem ipsum dolor, sit amet consectetur adipisicing elit. Accusantium
+          nostrum officiis provident quos nihil dicta rerum, minima debitis
+          exercitationem explicabo ex praesentium perferendis porro numquam
+          earum aspernatur sed, molestias quis recusandae. Ullam ratione
+          tempore, amet pariatur sed nulla totam minima eius a corporis quod,
+          dolorum magni cum quam, optio facere officia debitis soluta quia
+          deserunt! Error ex dolorum, incidunt quasi optio et sunt iusto est
+          similique impedit illum. Voluptates dolorum mollitia dignissimos non
+          natus, blanditiis ad molestias ut officia incidunt nihil magnam enim.
+          Explicabo rerum totam pariatur eaque minima impedit quisquam. Porro
+          alias sit sint placeat. Ut labore tempore in.
+        </p>
+        <p>
+          Lorem ipsum dolor, sit amet consectetur adipisicing elit. Accusantium
+          nostrum officiis provident quos nihil dicta rerum, minima debitis
+          exercitationem explicabo ex praesentium perferendis porro numquam
+          earum aspernatur sed, molestias quis recusandae. Ullam ratione
+          tempore, amet pariatur sed nulla totam minima eius a corporis quod,
+          dolorum magni cum quam, optio facere officia debitis soluta quia
+          deserunt! Error ex dolorum, incidunt quasi optio et sunt iusto est
+          similique impedit illum. Voluptates dolorum mollitia dignissimos non
+          natus, blanditiis ad molestias ut officia incidunt nihil magnam enim.
+          Explicabo rerum totam pariatur eaque minima impedit quisquam. Porro
+          alias sit sint placeat. Ut labore tempore in.
+        </p>
+        <p>
+          Lorem ipsum dolor, sit amet consectetur adipisicing elit. Accusantium
+          nostrum officiis provident quos nihil dicta rerum, minima debitis
+          exercitationem explicabo ex praesentium perferendis porro numquam
+          earum aspernatur sed, molestias quis recusandae. Ullam ratione
+          tempore, amet pariatur sed nulla totam minima eius a corporis quod,
+          dolorum magni cum quam, optio facere officia debitis soluta quia
+          deserunt! Error ex dolorum, incidunt quasi optio et sunt iusto est
+          similique impedit illum. Voluptates dolorum mollitia dignissimos non
+          natus, blanditiis ad molestias ut officia incidunt nihil magnam enim.
+          Explicabo rerum totam pariatur eaque minima impedit quisquam. Porro
+          alias sit sint placeat. Ut labore tempore in.
+        </p>
+        <p>
+          Lorem ipsum dolor, sit amet consectetur adipisicing elit. Accusantium
+          nostrum officiis provident quos nihil dicta rerum, minima debitis
+          exercitationem explicabo ex praesentium perferendis porro numquam
+          earum aspernatur sed, molestias quis recusandae. Ullam ratione
+          tempore, amet pariatur sed nulla totam minima eius a corporis quod,
+          dolorum magni cum quam, optio facere officia debitis soluta quia
+          deserunt! Error ex dolorum, incidunt quasi optio et sunt iusto est
+          similique impedit illum. Voluptates dolorum mollitia dignissimos non
+          natus, blanditiis ad molestias ut officia incidunt nihil magnam enim.
+          Explicabo rerum totam pariatur eaque minima impedit quisquam. Porro
+          alias sit sint placeat. Ut labore tempore in.
+        </p>
+        <p>
+          Lorem ipsum dolor, sit amet consectetur adipisicing elit. Accusantium
+          nostrum officiis provident quos nihil dicta rerum, minima debitis
+          exercitationem explicabo ex praesentium perferendis porro numquam
+          earum aspernatur sed, molestias quis recusandae. Ullam ratione
+          tempore, amet pariatur sed nulla totam minima eius a corporis quod,
+          dolorum magni cum quam, optio facere officia debitis soluta quia
+          deserunt! Error ex dolorum, incidunt quasi optio et sunt iusto est
+          similique impedit illum. Voluptates dolorum mollitia dignissimos non
+          natus, blanditiis ad molestias ut officia incidunt nihil magnam enim.
+          Explicabo rerum totam pariatur eaque minima impedit quisquam. Porro
+          alias sit sint placeat. Ut labore tempore in.
+        </p>
+        <p>
+          Lorem ipsum dolor, sit amet consectetur adipisicing elit. Accusantium
+          nostrum officiis provident quos nihil dicta rerum, minima debitis
+          exercitationem explicabo ex praesentium perferendis porro numquam
+          earum aspernatur sed, molestias quis recusandae. Ullam ratione
+          tempore, amet pariatur sed nulla totam minima eius a corporis quod,
+          dolorum magni cum quam, optio facere officia debitis soluta quia
+          deserunt! Error ex dolorum, incidunt quasi optio et sunt iusto est
+          similique impedit illum. Voluptates dolorum mollitia dignissimos non
+          natus, blanditiis ad molestias ut officia incidunt nihil magnam enim.
+          Explicabo rerum totam pariatur eaque minima impedit quisquam. Porro
+          alias sit sint placeat. Ut labore tempore in.
+        </p>
+        <p>
+          Lorem ipsum dolor, sit amet consectetur adipisicing elit. Accusantium
+          nostrum officiis provident quos nihil dicta rerum, minima debitis
+          exercitationem explicabo ex praesentium perferendis porro numquam
+          earum aspernatur sed, molestias quis recusandae. Ullam ratione
+          tempore, amet pariatur sed nulla totam minima eius a corporis quod,
+          dolorum magni cum quam, optio facere officia debitis soluta quia
+          deserunt! Error ex dolorum, incidunt quasi optio et sunt iusto est
+          similique impedit illum. Voluptates dolorum mollitia dignissimos non
+          natus, blanditiis ad molestias ut officia incidunt nihil magnam enim.
+          Explicabo rerum totam pariatur eaque minima impedit quisquam. Porro
+          alias sit sint placeat. Ut labore tempore in.
+        </p>
+      </div>
     </section>
   );
 };
