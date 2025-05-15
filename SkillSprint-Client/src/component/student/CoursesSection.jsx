461d7c822176805f113997d70fb931f0cdb13dee
almamun528 <ayapths@gmail.com>
2025-05-16 00:46:09 +0600

    add course dummy object

--- SkillSprint-Client/src/component/student/CoursesSection.jsx
+++ SkillSprint-Client/src/component/student/CoursesSection.jsx
@@ -1,5 +1,19 @@
+import { Link } from "react-router-dom";
+
 const CoursesSection = () => {
-  return <div>Courses Section</div>;
+  return (
+    <section>
+      <h2>Title</h2>
+      <p>
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Deserunt
+        mollitia perferendis perspiciatis quaerat voluptas magnam officiis fuga
+        labore error. Doloremque?
+      </p>
+      <Link to={"/course-list"} onClick={() => scrollTo(0, 0)}>
+        Show All Course
+      </Link>
+    </section>
+  );
 };
 
 export default CoursesSection;
--- SkillSprint-Client/src/pages/student/Home.jsx
+++ SkillSprint-Client/src/pages/student/Home.jsx
@@ -1,4 +1,5 @@
 import Slider from "../../component/Slider/Slider";
+import CoursesSection from "../../component/student/CoursesSection";
 import SearchBar from "../../component/student/SearchBar";
 // import useUser from "../../hook/useUser";
 
@@ -10,6 +11,8 @@ const Home = () => {
       <Slider />
       <br />
       <SearchBar/>
+      <br />
+      <CoursesSection/>
     </section>
   );
 };
